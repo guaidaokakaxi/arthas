cce7009aff7b954678e4faac37fcc474f22083ca
guaidaokakaxi <guaidaokakaxi@gmail.com>
2021-01-14 14:46:31 +0800

    delete cache variable FOUND_JAVA_HOME

--- boot/src/main/java/com/taobao/arthas/boot/ProcessUtils.java
+++ boot/src/main/java/com/taobao/arthas/boot/ProcessUtils.java
@@ -31,8 +31,6 @@ import com.taobao.arthas.common.PidUtils;
  *
  */
 public class ProcessUtils {
-    private static String FOUND_JAVA_HOME = null;
-
     //status code from com.taobao.arthas.client.TelnetConsole
     /**
      * Process success
@@ -179,10 +177,6 @@ public class ProcessUtils {
      * @return
      */
     public static String findJavaHome() {
-        if (FOUND_JAVA_HOME != null) {
-            return FOUND_JAVA_HOME;
-        }
-
         String javaHome = System.getProperty("java.home");
 
         if (JavaVersionUtils.isLessThanJava9()) {
@@ -196,8 +190,7 @@ public class ProcessUtils {
             }
 
             if (toolsJar.exists()) {
-                FOUND_JAVA_HOME = javaHome;
-                return FOUND_JAVA_HOME;
+                return javaHome;
             }
 
             if (!toolsJar.exists()) {
@@ -215,17 +208,14 @@ public class ProcessUtils {
 
                 if (toolsJar.exists()) {
                     AnsiLog.info("Found java home from System Env JAVA_HOME: " + javaHomeEnv);
-                    FOUND_JAVA_HOME = javaHomeEnv;
-                    return FOUND_JAVA_HOME;
+                    return javaHomeEnv;
                 }
 
                 throw new IllegalArgumentException("Can not find tools.jar under java home: " + javaHome
                                 + ", please try to start arthas-boot with full path java. Such as /opt/jdk/bin/java -jar arthas-boot.jar");
             }
-        } else {
-            FOUND_JAVA_HOME = javaHome;
         }
-        return FOUND_JAVA_HOME;
+        return javaHome;
     }
 
     public static void startArthasCore(long targetPid, List<String> attachArgs) {
